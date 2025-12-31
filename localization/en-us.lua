797ab9131a8afa6ce91cd410acdabe3ea1480511
tepid <tepschmep@gmail.com>
2025-12-31 11:51:56 -0300

    add localization boilerplate for en-us

---
+++ localization/en-us.lua
@@ -0,0 +1,35 @@
+return {
+    descriptions = {
+        Back = {},
+        Blind = {},
+        Edition = {},
+        Enhanced = {},
+        Joker = {},
+        Other = {},
+        Planet = {},
+        Spectral = {},
+        Stake = {},
+        Tag = {},
+        Tarot = {},
+        Voucher = {}
+    },
+    misc = {
+        achievement_descriptions = {},
+        achievement_names = {},
+        blind_states = {},
+        challenge_names = {},
+        collabs = {},
+        dictionary = {},
+        high_scores = {},
+        labels = {},
+        poker_hand_descriptions = {},
+        poker_hands = {},
+        quips = {},
+        ranks = {},
+        suits_plural = {},
+        suits_singular = {},
+        tutorial = {},
+        v_dictionary = {},
+        v_text = {}
+    },
+}
