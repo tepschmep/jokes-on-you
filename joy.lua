9756a93fac14b80fa8cca5ada72d2f7013e257a8
tepid <tepschmep@gmail.com>
2026-01-01 22:01:03 -0300

    refactored load function to load all jokers and their atlas

--- joy.lua
+++ joy.lua
@@ -1,5 +1,42 @@
-local function load(path)
-    assert(SMODS.load_file(path))()
+local function load(arg)
+    if type(arg) == "string" then
+        assert(SMODS.load_file(arg))()
+        return
+    end
+
+    if type(arg) == "table" then
+        -- Load the atlas
+        if arg.atlas then SMODS.Atlas(arg.atlas) end
+
+        -- Load the files
+        local dir = "src/" .. arg.type .. "/"
+        for _, filename in ipairs(arg.filenames) do load(dir .. filename .. ".lua") end
+
+        return
+    end
+
+    error("undefined call")
 end
 
-load "src/jokers.lua"
+load {
+    type = "jokers",
+    atlas = {
+        key = "jokers",
+        path = "jokers.png",
+        px = 71,
+        py = 95
+    },
+    filenames = {
+        -- Common Jokers
+        "command_tower",
+        "jimbazzaro",
+
+        -- Uncommon Jokers
+        "down_the_drain",
+        "macaroni_art",
+
+        -- Rare Jokers
+        "immortal_snail",
+        "paddleball"
+    }
+}
