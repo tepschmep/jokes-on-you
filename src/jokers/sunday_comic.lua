d74d51eecd1b660139495bbc1c017478bd626b3f
yoshikirby26 <zmartkid26@gmail.com>
2026-01-06 18:55:40 -0500

    patched Sunday Comic joker (tag_key scope issue)

--- src/jokers/sunday_comic.lua
+++ src/jokers/sunday_comic.lua
@@ -32,22 +32,24 @@ SMODS.Joker
 
     calculate = function(self, card, context)
         local config = card.ability.extra
+        local tag_key = ""
 
         if context.setting_blind and G.GAME.blind:get_type() == "Small" then
-	    for i = 1, config.tag_amount do
+	        for i = 1, config.tag_amount do
                 repeat
-                    local tag_key = get_next_tag_key()
-		until tag_key ~= "tag_boss" and tag_key ~= "tag_orbital"
+                    tag_key = get_next_tag_key()
+		        until tag_key ~= "tag_boss" and tag_key ~= "tag_orbital"
+    
+                add_tag(Tag(tag_key))
+            end
 
 		-- potential TODO: orbital tags can be added but it takes a fair bit to do
 		-- if you wanna go for it, reference Pity Prize from Cryptid
-		add_tag(Tag(tag_key))
-	    end
-
-            return {
-                message = localize { type = "variable", key = "v_j_o_y_plus_tag", vars = { config.tag_amount } },
-                colour = G.C.FILTER
-            }
+        
+        return {
+            message = localize { type = "variable", key = "v_j_o_y_plus_tag", vars = { config.tag_amount } },
+            colour = G.C.FILTER
+        }
         end
     end
 }
